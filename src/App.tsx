1d8e64c54c5524007f435c93ca70f17d4633d6df
Borisov Semen <devsnice@gmail.com>
2018-11-08 17:10:07 +0300

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import Header from "./common.blocks/Header";
+
+import "./App.scss";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
